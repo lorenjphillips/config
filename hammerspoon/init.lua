11e8de0f8995b0c1be1995f5fa4fc48816135a3c
Loren Phillips <lorenphillips@protonmail.com>
2026-05-03 09:55:33 -0700

    hammerspoon: change hotkey to ctrl+shift+w

--- hammerspoon/init.lua
+++ hammerspoon/init.lua
@@ -1,6 +1,6 @@
--- Global hotkey: open a new Warp window wherever the cursor is
+-- Global hotkey (ctrl+shift+w): open a new Warp window wherever the cursor is
 -- Requires Warp to be set to "All Desktops" in the Dock (right-click → Options → All Desktops)
-hs.hotkey.bind({"cmd", "shift"}, "return", function()
+hs.hotkey.bind({"ctrl", "shift"}, "w", function()
   local warp = hs.application.find("dev.warp.Warp-Stable")
   if warp then
     hs.osascript.applescript([[
