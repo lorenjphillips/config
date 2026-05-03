e3eb0df057be75a32ed351bb2ce863140fc1a4b2
Loren Phillips <lorenphillips@protonmail.com>
2026-05-03 09:57:55 -0700

    hammerspoon: use menu click instead of keystroke, add launch delay

--- hammerspoon/init.lua
+++ hammerspoon/init.lua
@@ -1,15 +1,22 @@
 -- Global hotkey (ctrl+option+w): open a new Warp window wherever the cursor is
 -- Requires Warp to be set to "All Desktops" in the Dock (right-click → Options → All Desktops)
+
+local function newWarpWindow()
+  hs.osascript.applescript([[
+    tell application "System Events"
+      tell process "Warp"
+        click menu item "New Window" of menu "File" of menu bar 1
+      end tell
+    end tell
+  ]])
+end
+
 hs.hotkey.bind({"ctrl", "alt"}, "w", function()
   local warp = hs.application.find("dev.warp.Warp-Stable")
   if warp then
-    hs.osascript.applescript([[
-      tell application "Warp" to activate
-      tell application "System Events"
-        keystroke "n" using {command down}
-      end tell
-    ]])
+    newWarpWindow()
   else
     hs.application.open("dev.warp.Warp-Stable")
+    hs.timer.doAfter(2, newWarpWindow)
   end
 end)
